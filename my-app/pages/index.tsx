8b175d3b265d3c514760ce40bd8cbcd09ffa7d62
nguyenbaduy <105105870+nguyenbaduy0082@users.noreply.github.com>
2022-11-03 17:44:17 +0700

    Feat/hero icon installation (#8)
    * feat: build home page
    
    * feat: create header component
    
    * feat: tailwind custom class
    
    * feat: react icon installation
    
    Co-authored-by: An Le Dai <55825127+ldan2002@users.noreply.github.com>

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,17 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
+
     <div className="flex min-h-screen float-col items-center justify-center py-2" >
+
       <Head>
         <title>
           Home - Netflix
         </title>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
-      Lets build Netflix
-      {/* Header */}
       <main>
         {/* Banner */}
         <section>
@@ -25,6 +26,7 @@ const Home: NextPage = () => {
       </main>
       {/* Modal */}
     </div>
+
   )
 }
 
