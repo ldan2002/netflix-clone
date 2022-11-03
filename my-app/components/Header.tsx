2d6a2406acc574c4dbe6cb5a0c4ed118701d4c81
nguyenbaduy0082 <nguyenbaduy0082@gmail.com>
2022-11-03 16:44:25 +0700

    feat: tailwind custom class

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -12,15 +12,15 @@ function Header() {
                 />
 
                 <ul className="hidden space-x-4 md:flex">
-                    <li>Home</li>
-                    <li>TV Shows</li>
-                    <li>Movies</li>
-                    <li>New & Popular</li>
-                    <li>My List</li>
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">TV Shows</li>
+                    <li className="headerLink">Movies</li>
+                    <li className="headerLink">New & Popular</li>
+                    <li className="headerLink">My List</li>
                 </ul>
             </div>
             <div>
-
+                
             </div>
         </header>
     )
