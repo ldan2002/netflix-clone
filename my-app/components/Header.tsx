282eac2d92e9e286f0dc8bf0266b304abe2eef24
nguyenbaduy0082 <nguyenbaduy0082@gmail.com>
2022-11-03 16:14:04 +0700

    feat: create header component

---
+++ my-app/components/Header.tsx
@@ -0,0 +1,29 @@
+
+
+function Header() {
+    return (
+        <header>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                    width={100}
+                    height={100}
+                    className="cursor-pointer object-contain"
+                />
+
+                <ul className="hidden space-x-4 md:flex">
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                </ul>
+            </div>
+            <div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex min-h-screen float-col items-center justify-center py-2" >
+      <div className="" >
       <Head>
         <title>
           Home - Netflix
         </title>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
-      Lets build Netflix
-      {/* Header */}
+      <Header />
       <main>
         {/* Banner */}
         <section>
