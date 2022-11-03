9eb79f1285680a0e9424b56bc62c03b3088583eb
nguyenbaduy0082 <nguyenbaduy0082@gmail.com>
2022-11-03 13:56:12 +0700

    feat: build home page

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -3,9 +3,28 @@ import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <>
-
-    </>
+    <div className="flex min-h-screen float-col items-center justify-center py-2" >
+      <Head>
+        <title>
+          Home - Netflix
+        </title>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      Lets build Netflix
+      {/* Header */}
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
+    </div>
   )
 }
 
